aedbc161ce54e8d8b5061b6d9a4552c4f080b352
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2023-07-21 15:31:10 +0300

    bg fix

--- components/Tasks/Tasks.tsx
+++ components/Tasks/Tasks.tsx
@@ -2,11 +2,14 @@ import tasksData from "./tasksData";
 import TasksList from "./TasksList/TasksList";
 import SectionContainer from "../SectionContainer/SectionContainer";
 
+import bg from "@/public/images/tasks/task-bg.jpg";
+console.log(bg);
+
 const Tasks: React.FC = () => {
   return (
     <SectionContainer
       title="Основні завдання"
-      bgImg="/images/tasks/task-bg.jpg"
+      bgImg={bg.src}
       titleColor="var(--task-text-color)"
       centerTitle
     >
